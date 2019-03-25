02f6971518e515066ee99e53b54e114d5da0b998
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-24 17:58:53 -0700

    hmmm

--- StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Classes/Game.cs
+++ StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Classes/Game.cs
@@ -99,7 +99,7 @@ namespace Lab04_TicTacToe.Classes
 
 
 
-                //IM STUCK
+                //IM STUCK i have been struggling with this all day today i cannot make snese of this.
                 
 			
 			}
