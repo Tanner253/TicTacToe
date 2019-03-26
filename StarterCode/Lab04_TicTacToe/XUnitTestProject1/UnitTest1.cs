7c4acd07d4792c3bb111f2a7e0a0962958aba531
Tanner <Percivaltanner@gmail.comgit add index.html$ git add index.htmlgit config --global user.Email Percivaltanner@gmail.com>
2019-03-26 11:02:31 -0700

    adds testing proj file

---
+++ StarterCode/Lab04_TicTacToe/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+
+        /// <summary>
+        /// TEST FOR : Winners
+        /// switch player works
+        ///confirm position with index array
+        ///1 unique test
+        ///will come back to this!
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
